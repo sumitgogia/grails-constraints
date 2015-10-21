236f36640b6e11965b3db1e5b53262f5ccdc8212
sumit <sumit@cloudzilla.com>
2015-10-21 13:08:30 +0530

    Fix 'association references unmapped class: java.util.List'
    * Tell hibernate how to map List via a hasMany block.

--- grails-app/domain/net/zorched/test/Foo.groovy
+++ grails-app/domain/net/zorched/test/Foo.groovy
@@ -2,7 +2,9 @@ package net.zorched.test
 
 class Foo {
     String bar
-    List baz
+    List<String> baz
+
+    static hasMany = [baz: String]
 
     static constraints = {
         bar(twoLong: true, nullable:true)
